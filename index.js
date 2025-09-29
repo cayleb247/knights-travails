95406fc34e4ed77b8c42844e757f91e15f51cf5d
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-09-28 22:42:13 -0400

    First iteration
    BDS for the end square

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
-class moveNode {
+class KnightTravailer {
   constructor(startSquare, endSquare) {
     this.startSquare = startSquare;
     this.endSquare = endSquare;
+    this.rootMove = new moveNode(null, startSquare);
+  }
+  findEndSquare() {
+    let moveQueue = [this.rootMove];
+    let endSquareMoveNode = null;
+
+    while (endSquareFound == null) {
+      let currentMoveNode = moveQueue[0];
+      if (currentMoveNode.startSquare != this.endSquare) {
+        for (const move of currentMoveNode.availableMoves) {
+          let newMoveNode = new moveNode(currentMoveNode, move);
+          moveQueue.push(newMoveNode);
+        }
+        moveQueue.shift();
+      } else {
+        endSquareFound = currentMoveNode;
+      }
+    }
+
+    return  endSquareMoveNode;
+  }
+
+  getMovePath(endNode) {
+    let movePath = [];
+    let currentNode = endNode;
+    while (currentNode.prevSquare !== null) {
+      movePath.append(currentNode.startSquare);
+      currentNode = currentNode.prevSquare;
+    }
+    movePath.append(currentNode.startSquare);
+
+    return movePath.reverse();
+    
+  }
+}
+
+class moveNode {
+  constructor(prevSquare, startSquare) {
+    this.startSquare = startSquare
+    this.previousSquare = prevSquare;
     this.availableMoves = calculateMoves();
     this.oneMoveAway = false;
   }
@@ -16,7 +56,7 @@ class moveNode {
     availableMoves.push([xCord + 2, yCord - 1]);
     availableMoves.push([xCord - 2, yCord + 1]);
     availableMoves.push([xCord - 2, yCord - 1]);
-    availableMoves.push([xCord + 1, yCord - 2]);
+    availableMoves.push(xCord + 1, yCord - 2);
     availableMoves.push([xCord - 1, yCord - 2]);
 
     availableMoves = availableMoves.filter(
@@ -32,27 +72,6 @@ class moveNode {
   }
 }
 
-function calculateMoves(startSquare) {
-  let availableMoves = [];
-  let xCord = startSquare[0];
-  let yCord = startSquare[1];
-
-  availableMoves.push([xCord + 1, yCord + 2]);
-  availableMoves.push([xCord - 1, yCord + 2]);
-  availableMoves.push([xCord + 2, yCord + 1]);
-  availableMoves.push([xCord + 2, yCord - 1]);
-  availableMoves.push([xCord - 2, yCord + 1]);
-  availableMoves.push([xCord - 2, yCord - 1]);
-  availableMoves.push([xCord + 1, yCord - 2]);
-  availableMoves.push([xCord - 1, yCord - 2]);
-
-  availableMoves = availableMoves.filter(
-    (array) => array[0] >= 0 && array[0] <= 7 && array[1] >= 0 && array[1] <= 7
-  );
-
-  return availableMoves;
-}
-
 function knightMoves(startSquare, endSquare) {
   if (typeof startSquare != "object" || typeof endSquare != "object") {
     return "Invalid input: Not an array";
@@ -60,7 +79,8 @@ function knightMoves(startSquare, endSquare) {
     return "Invalid input: Incorrect shape";
   }
 
-  return calculateMoves(startSquare);
+  const knightTravailer = new KnightTravailer(startSquare, endSquare);
+  return knightTravailer.findEndSquare();
 }
 
 console.log(knightMoves([0, 0], [3, 5]));
